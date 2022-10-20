1013676ab5b0645a3b3f7273bfdd8f552f8ff2e5
taji <taji.ac01@gmail.com>
2022-10-20 16:51:02 +0900

    VA

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          WA3aaWelcome to <a href="https://nextjs.org">Next.js!</a>
+          WA3aabWelcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
